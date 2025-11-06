d3da9f25c82577b48b9a703692915e20bd578890
Amraa1 <amarsanaaganbaatar0409@gmail.com>
2025-11-06 14:05:42 +0800

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/new-year-2025",
+  base: "/new-year-2025/",
   plugins: [react(), tailwindcss()],
 });
